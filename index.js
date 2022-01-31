8bdff3f3bcc43c78ee0eea1dea46f48cfbcc8684
Furkan Özelge <52843073+furkanozelge@users.noreply.github.com>
2022-02-01 02:57:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el");
+
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    renderLeads();
+    
+
+})
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i<myLeads.length;i++){
+        let httpControl = myLeads[i];
+        //if(httpControl.substring(0,7) != "https://"){
+          //  listItems+="<li><a target='_blank' href='" +"https://" +myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+            //console.log(listItems)
+        //}
+        //else{
+            listItems+="<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+            console.log(listItems)
+        //}
+
+}
+    ulEl.innerHTML = listItems;
+}
+
+
